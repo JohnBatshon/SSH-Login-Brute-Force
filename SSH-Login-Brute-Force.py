2bcd5f7fd9f3134b322e52798f7b05aa73bd9463
John Batshon <142701108+JohnBatshon@users.noreply.github.com>
2023-09-04 09:24:43 -0700

    Update SSH-Login-Brute-Force.py

--- SSH-Login-Brute-Force.py
+++ SSH-Login-Brute-Force.py
@@ -4,6 +4,21 @@
 #
 # ####################################################################################
 #
+# Disclaimer: SSH Login Brute Force Script
 #
+# This script is provided for educational and testing purposes only. It is intended to be used responsibly and legally, solely on your own machine or on systems for which you have obtained explicit, written authorization from the system owner or administrator.
+#
+# Usage Guidelines:
+#
+# Responsible Use: Please use this script in a responsible and ethical manner. Unauthorized access to computer systems, networks, or devices is illegal and unethical.
+# Authorized Access: Before using this script on any system that you do not own or administer, ensure you have obtained written authorization from the system owner or authorized personnel. Unauthorized access is a violation of privacy and may result in legal consequences.
+# Educational Purposes: This script is intended for educational purposes to understand security vulnerabilities and should not be used maliciously.
+# Legal Compliance: Comply with all applicable laws and regulations regarding computer security and data privacy in your jurisdiction.
+# By using this script, you acknowledge and agree to abide by these guidelines and accept full responsibility for any consequences resulting from its use.
+#
+# The creator of this script and any affiliated parties shall not be held liable for any misuse, damage, or legal actions resulting from the use of this script in violation of these guidelines.
+#
+# Please note that even with a disclaimer, the use of such a script may still be illegal or unethical in certain situations, so always exercise caution and adhere to legal and ethical standards. It's essential to respect privacy and obtain proper authorization when testing or using security tools like this.
 #
 # ####################################################################################
+
